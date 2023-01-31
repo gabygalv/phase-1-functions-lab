18fecddc0bd553e65b4923468f3d67b7e7969a4c
Gabriela Galvan <gabriela1galvan@gmail.com>
2023-01-31 10:57:24 -0700

    funcitons lab completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (blocks) {
+    return Math.abs(42 - blocks);
+}
+ 
+function distanceFromHqInFeet (feet) {
+    let blocks = distanceFromHqInBlocks(feet); 
+    return Math.abs(264 * blocks);
+}
+
+function distanceTravelledInFeet (start, destination) {
+    return Math.abs(start - destination)*264;
+}
+
+function calculatesFarePrice (start, destination) {
+    const distance = distanceTravelledInFeet (start, destination);
+    if (distance <= 400)  {
+        return 0
+    }
+    else if (distance > 400 && distance <= 2000) {
+       return ((distance - 400) * .02)
+    }
+    else if (distance >2000 & distance <2500) {
+        return 25
+    }
+    else (distance >= 2500) ;
+        return 'cannot travel that far';
+    }
